fc577780b585e3f7ebee4ad861b838b40b307e86
NicoDemosaS <nicolasgabrielbrunismann6@hotmail.com>
2025-04-08 23:50:13 -0300

    exercicio04 feito

---
+++ Exercicio04.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int maior(int*vetor, int tam){
+    int i, maior;
+    maior = 0;
+    for(i=0;i<tam;i++){
+        if(vetor[i] > maior)
+            maior = vetor[i];
+    }
+    return maior;
+}
+
+
+int main(){
+    int n, i;
+    int *p;
+    printf("Quantos numeros a alocar: ");
+    scanf("%d", &n);
+    p = malloc(sizeof(int) * n);
+    for(i=0;i<n;i++){
+        scanf("%d", p+i);
+    }
+
+    // Printando o vetor.
+    for(i=0;i<n;i++){
+        printf("%d \n", *(p+i));
+    }
+
+    printf("%d", maior(p, n));
+    return 0;
+}
