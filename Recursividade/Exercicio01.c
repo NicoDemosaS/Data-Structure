029b42b9d6f95214d120f7edd63c7634f3046956
NicoDemosaS <nicolasgabrielbrunismann6@hotmail.com>
2025-04-26 22:56:29 -0300

    primeiro exercicio

---
+++ Recursividade/Exercicio01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    int soma(int x, int y){
+        if(y == 0)
+            return x;
+        if(y > 0){
+            return soma(x+1, y-1);
+        }
+    }
+
+    printf("%d", soma(5, 5));
+
+
+
+
+}
